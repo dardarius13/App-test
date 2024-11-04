ad04ca706ad983e2063cfbe540bf1b6a3fe604c4
Dario Saldamarco <dario.saldamarco@gmail.com>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ App test/App_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_testApp.swift
+//  App test
+//
+//  Created by Dario Saldamarco on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct App_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
